a44d77e08f948124632ce5c5724f059013820fc6
Rahul M <your_email@example.com>
2026-01-31 00:04:17 +0530

    cleared the issued with the producation cors orgin issue from frontend

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,10 +1,13 @@
 // frontend/src/api/config.js
 
+// // This variable checks if we are on the internet (production) or on your laptop (development)
+// export const API_BASE_URL = import.meta.env.MODE === "production"
+//   // ? "https://axon-hire-backend.onrender.com" // We will update this real URL later if it changes
+//   ?"https://axon-hire-mvp.onrender.com"
+//   : "http://localhost:5000"; 
+
 // This variable checks if we are on the internet (production) or on your laptop (development)
-export const API_BASE_URL = import.meta.env.MODE === "production"
-  // ? "https://axon-hire-backend.onrender.com" // We will update this real URL later if it changes
-  ?"https://axon-hire-mvp.onrender.com"
-  : "http://localhost:5000"; 
+export const API_BASE_URL ="https://axon-hire-mvp.onrender.com"
 
 // We export it so other files (like Profile.jsx) can use it.
 export const BACKEND_URL = API_BASE_URL;
